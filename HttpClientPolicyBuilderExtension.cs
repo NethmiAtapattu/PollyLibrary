a9c2b1ed9a5e37ecde93b8bafd1adafe88e1e165
NethmiAtapattu <nethmiatapattu97@gmail.com>
2020-09-29 13:55:36 +0530

    Initial addings

---
+++ HttpClientPolicyBuilderExtension.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DataIntegration.HttpPolicies
+{
+    public static class HttpClientPolicyBuilderExtension
+    {
+        public static IHttpClientBuilder ForwardPolicyHandlers(this IHttpClientBuilder builder)
+        {
+            if (builder is null) throw new ArgumentNullException(nameof(builder));
+            var httpPolicyHandler = new HttpClientPolicyHandler();
+            builder.Services.AddPolicyRegistry(httpPolicyHandler.HttpPolicyRegistry);
+            builder.AddPolicyHandlerFromRegistry(httpPolicyHandler.PolicySelector);
+            return builder;
+        }
+    }
+}
---
+++ HttpClientPolicyHandler.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Linq;
+using System.Net.Http;
+using Polly;
+using Polly.Extensions.Http;
+using Polly.Registry;
+
+namespace DataIntegration.HttpPolicies
+{
+    internal class HttpClientPolicyHandler
+    {
+        public HttpClientPolicyHandler()
+        {
+            HttpPolicyRegistry = new PolicyRegistry
+            {
+                {"NoOpPolicy", NoOpPolicy},
+                {"WaitAndRetryPolicy", WaitAndRetryPolicy},
+                {"CircuitBreakerPolicy", GetCircuitBreakerPolicy()},
+                {"CircuitBreakerWithWaitAndRetryPolicy", CircuitBreakerWithWaitAndRetryPolicy}
+            };
+        }
+
+        public PolicyRegistry HttpPolicyRegistry { get; }
+
+        private IAsyncPolicy<HttpResponseMessage> NoOpPolicy => Policy.NoOpAsync()
+            .AsAsyncPolicy<HttpResponseMessage>();
+
+        private IAsyncPolicy<HttpResponseMessage> WaitAndRetryPolicy => Policy
+            .Handle<HttpRequestException>()
+            .OrResult<HttpResponseMessage>(r => !r.IsSuccessStatusCode)
+            .WaitAndRetryAsync(4, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)));
+
+       
+        private IAsyncPolicy<HttpResponseMessage> CircuitBreakerWithWaitAndRetryPolicy =>
+            Policy.WrapAsync(WaitAndRetryPolicy, GetCircuitBreakerPolicy());
+
+        private IAsyncPolicy<HttpResponseMessage> GetCircuitBreakerPolicy()
+        {
+            return HttpPolicyExtensions
+                .HandleTransientHttpError()
+                .CircuitBreakerAsync(5, TimeSpan.FromSeconds(30), OnBreak, OnReset, OnHalfOpen);
+        }
+
+
+
+        public IAsyncPolicy<HttpResponseMessage> PolicySelector(IReadOnlyPolicyRegistry<string> policyRegistry, HttpRequestMessage httpRequestMessage)
+        {
+            try
+            {
+                if (httpRequestMessage.Method == HttpMethod.Get)
+                {
+                    return policyRegistry.Get<IAsyncPolicy<HttpResponseMessage>>("CircuitBreakerWithWaitAndRetryPolicy");
+                }
+                else if (httpRequestMessage.Method == HttpMethod.Post && httpRequestMessage.Method == HttpMethod.Put)
+                {
+                    return policyRegistry.Get<IAsyncPolicy<HttpResponseMessage>>("CircuitBreakerWithWaitAndRetryPolicy");
+                }
+                else
+                {
+                    return policyRegistry.Get<IAsyncPolicy<HttpResponseMessage>>("NoOpPolicy");
+                }
+            }
+            catch (ArgumentNullException)
+            {
+                return policyRegistry.Get<IAsyncPolicy<HttpResponseMessage>>("NoOpPolicy");
+            }
+
+        }
+
+        private static void OnReset()
+        {
+            Console.WriteLine("RESET");
+            Console.WriteLine();
+        }
+
+        private static void OnHalfOpen()
+        {
+            Console.WriteLine("HALFOPEN");
+            Console.WriteLine();
+        }
+
+        private static void OnBreak(DelegateResult<HttpResponseMessage> arg1, TimeSpan arg2)
+        {
+            Console.WriteLine("BREAK");
+            Console.WriteLine();
+        }
+    }
+}
